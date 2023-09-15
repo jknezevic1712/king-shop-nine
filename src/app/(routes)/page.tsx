22c51a2fdb1cdbaa1df6adc288d7195fb1b89ee1
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-09-15 22:23:51 +0200

    Naming changed.

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,7 +2,7 @@
 // import { getServerSession } from 'next-auth/next';
 import Button from '../_components/atoms/button/button';
 import FormField from '../_components/molecules/formField/formField';
-import HomeTemplate from '../_components/templates/home/home';
+import HomeTemplate from '../_components/templates/homeTemplate/homeTemplate';
 
 export default async function Home() {
 	// const session = await getServerSession(authOptions);
