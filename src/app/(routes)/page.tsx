b2efe91bce826e658289de26e25314fd3cef0c5c
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-09-23 21:38:45 +0200

    Refactored home page using hoc.

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,18 +3,33 @@
 import { useEffect } from 'react';
 // components
 import HomeTemplate from '../_components/templates/homeTemplate/homeTemplate';
+import LoadingSpinner from '../_components/atoms/loadingSpinner/loadingSpinner';
 // hooks
 import { useFetchAllProducts } from '../_hooks/products/useFetchAllProducts';
-import { useAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
+// store
 import { productsAtom } from '../_store/store';
 
-export default function Home() {
-	const allProducts = useFetchAllProducts();
-	const [_, setAllProducts] = useAtom(productsAtom);
+function withDataFetching(Component: (props: any) => JSX.Element) {
+	return function DataFetchingComponent(props: any) {
+		const allProducts = useFetchAllProducts();
+		const setAllProducts = useSetAtom(productsAtom);
 
-	useEffect(() => {
-		setAllProducts(allProducts ?? []);
-	}, [allProducts]);
+		useEffect(() => {
+			setAllProducts(allProducts ?? []);
+		}, [allProducts]);
 
-	return <HomeTemplate products={allProducts} />;
+		if (!allProducts)
+			return (
+				<div className='flex h-full w-full items-center justify-center p-24'>
+					<LoadingSpinner size='lg' />
+				</div>
+			);
+
+		return <Component {...props} products={allProducts} />;
+	};
 }
+
+const Home = withDataFetching(HomeTemplate);
+
+export default Home;
--- src/app/_components/templates/homeTemplate/homeTemplate.tsx
+++ src/app/_components/templates/homeTemplate/homeTemplate.tsx
@@ -10,13 +10,6 @@ export default function HomeTemplate({
 }: {
 	products: Product[] | undefined;
 }) {
-	if (!products)
-		return (
-			<div className='flex h-full w-full items-center justify-center p-24'>
-				<LoadingSpinner size='lg' />
-			</div>
-		);
-
 	return (
 		<div className='mx-auto grid h-full max-w-7xl grid-cols-1 justify-items-center gap-6 p-4 xl:grid-cols-2'>
 			{products?.map((product, i) => <Card key={i} data={product} />)}
