42727c160d2f2f1415318a706035ed64245cc7d3
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-09-24 22:00:24 +0200

    Added query client persistance to cache data.

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { httpBatchLink } from '@trpc/react-query';
 import superjson from 'superjson';
+// Persist react query
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 import { trpc } from './client';
 import { env } from '@/env.mjs';
@@ -26,6 +29,7 @@ export default function TRPCProvider({
 				defaultOptions: {
 					queries: {
 						staleTime: 300 * 1000,
+						cacheTime: 1000 * 60 * 60 * 24,
 					},
 				},
 			})
@@ -40,6 +44,17 @@ export default function TRPCProvider({
 			transformer: superjson,
 		})
 	);
+
+	// Persist react query / cache data
+	if (typeof window !== 'undefined') {
+		const localStoragePersister = createSyncStoragePersister({
+			storage: window.localStorage,
+		});
+		persistQueryClient({
+			queryClient,
+			persister: localStoragePersister,
+		});
+	}
 
 	return (
 		<trpc.Provider client={trpcClient} queryClient={queryClient}>
